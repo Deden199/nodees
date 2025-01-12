b8e336337c90358c58b7ff796f7eceeacf3091cc
Adinhidayat <adin93273@gmail.com>
2025-01-12 20:02:32 +0700

    bismillah

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "off", // Nonaktifkan aturan untuk require
+    },
+  },
 ];
 
 export default eslintConfig;
